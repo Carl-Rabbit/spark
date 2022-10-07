ad95a17167c4711c2adeb0a51da9e444e133f44a
Carl-Rabbit <479843162@qq.com>
2022-10-07 15:39:12 +0800

    chore: add script

---
+++ mv_script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+
+cp ~/Documents/Code/Research/qotrace/spark-qotrace/sql/catalyst/target/spark-catalyst_2.12-3.3.0.jar ~/Applications/spark-3.3.0/jars/
+echo 'Copy spark jar finished'
